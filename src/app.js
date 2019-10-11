b6c7c536de245bda10ea33b2e1cf0dc9b21e9c0a
Uma <buldog52@gmail.com>
2019-10-11 23:04:25 +0300

    Addded .sticky menu

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 window.$ = window.jQuery = require('jquery');
 
 import './components/scroll-to-anchor/scroll-to-anchor';
+import './components/menu-toggle/menu-toggle';
 import './components/progressbar/progressbar';
 import './components/main-header/main-header';
 import './components/main-slider/main-slider';
-import './components/main-slider/main-slider';
+import './components/sticky-menu/sticky-menu';
 import './components/testimonials/testimonials';
 import './components/my-porfolio/my-porfolio';
 import './components/my-skills/my-skills';
