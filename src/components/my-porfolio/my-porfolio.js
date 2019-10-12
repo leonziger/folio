642444006a95771d118a3a920c619fc78b903171
Uma <buldog52@gmail.com>
2019-10-12 23:54:29 +0300

    added thumbs of all works to .my-portfolio

--- src/components/my-porfolio/my-porfolio.js
+++ src/components/my-porfolio/my-porfolio.js
@@ -1,31 +1,3 @@
 import $ from 'jquery';
 window.$ = window.jQuery = require('jquery');
 
-const jQueryBridget = require('jquery-bridget');
-const Isotope = require('isotope-layout');
-// make Isotope a jQuery plugin
-jQueryBridget('isotope', Isotope, $);
-// now you can use $().isotope()
-
-$('.my-portfolio__works').isotope({
-    itemSelector: '.my-portfolio__work',
-
-});
-
-$('.my-portfolio__works-menu-link').click(function(){
-
-    $('.my-portfolio__works-menu-link').removeClass('current-link');
-    $(this).addClass('current-link');
-    const selector = $(this).attr('data-filter');
-
-    $('.my-portfolio__works').isotope({
-        filter: selector,
-        animationOptions: {
-            duration: 1000,
-            easing: 'easeOutQuart',
-            queue: false
-        }
-    });
-    return false;
-
-});
